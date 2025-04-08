c5696cbf18fdc3ae31c5d567a0ac630798ec3f02
Adalberto R. Ribeiro <beto1821@uol.com.br>
2025-04-08 20:48:10 -0300

    CSS e inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import FormLogo from './assets/form-logo.png';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h3>OLa</h3>
-    </div>
+    <form>
+      <img src={FormLogo} alt="Form Logo" className="form-logo" />
+      
+      <label>
+        Nome
+        <input type="text" placeholder="Name" required />
+      </label>
+
+      <label>
+        Email
+        <input type="text" placeholder="Email" required />
+      </label>
+
+      <label>
+        Senha
+        <input type="text" placeholder="Senha" required />
+      </label>
+
+      <label>
+        Confirmar Senha
+        <input type="text" placeholder="Confirmar Senha" required />
+      </label>
+
+      <button type="submit">Cadastrar</button>
+    </form>
   );
 }
 
