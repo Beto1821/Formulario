be19f65a0814e2f7faccae38eb9fd08711cf5d74
Adalberto R. Ribeiro <beto1821@uol.com.br>
2025-04-08 20:33:19 -0300

    set up rev 1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <div>
-          <h3>OLa</h3>
-        </div>
-      </header>
+      <h3>OLa</h3>
     </div>
   );
 }
