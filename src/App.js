a3a1077790d2ff9d109b62300e8a7175b2a984fe
Adalberto R. Ribeiro <beto1821@uol.com.br>
2025-04-08 21:13:28 -0300

    Validation with react-hook-form

--- src/App.js
+++ src/App.js
@@ -1,31 +1,44 @@
 import React from 'react';
 import FormLogo from './assets/form-logo.png';
+import { useForm } from "react-hook-form"
 
 import './App.css';
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  function onSubmit(data) {
+    console.log(data)
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <img src={FormLogo} alt="Form Logo" className="form-logo" />
       
       <label>
         Nome
-        <input type="text" placeholder="Name" required />
+        <input type="text" placeholder="Nome" {...register("Name", { required: true })}/>
+        {errors.Name && <span>Campo obrigatório</span>}
       </label>
 
       <label>
         Email
-        <input type="text" placeholder="Email" required />
+        <input type="text" placeholder="Email" {...register("Email", { required: true })}/>
       </label>
 
       <label>
         Senha
-        <input type="text" placeholder="Senha" required />
+        <input type="text" placeholder="Senha" {...register("Senha", { required: true })}/>
       </label>
 
       <label>
         Confirmar Senha
-        <input type="text" placeholder="Confirmar Senha" required />
+        <input type="text" placeholder="Confirmar Senha" {...register("ConfirmarSenha", { required: true })}/>
       </label>
 
       <button type="submit">Cadastrar</button>
